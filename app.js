9afecc3f48dbbcf40c4473c17449f2df7004cfa9
fuadhasan3 <hasanfuad170@gmail.com>
2022-02-13 00:21:11 +0600

    let case number change

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 function updateCaseNumber(isIncreasing) {
     const caseInput = document.getElementById('case-number');
-    const caseNumber = caseInput.value;
+    let caseNumber = caseInput.value;
     if(isIncreasing == true){
-        caseInput.value = parseInt(caseNumber) + 1;
+        caseNumber = parseInt(caseNumber) + 1;
     }
     else if(caseNumber > 0) {
-        caseInput.value = parseInt(caseNumber) - 1;
+        caseNumber = parseInt(caseNumber) - 1;
     }
+    caseInput.value = caseNumber;
    // update case total
    const caseTotal =document.getElementById('case-total');
    caseTotal.innerText = caseNumber * 59;
